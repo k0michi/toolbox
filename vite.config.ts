741d1b947144c99f2cc5a8b66c6f9733c09037ae
Komichi <k0michi@koyomi.co>
2022-09-07 11:00:29 +0900

    Fix base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/toolbox/',
   plugins: [
     license({
       banner: {
